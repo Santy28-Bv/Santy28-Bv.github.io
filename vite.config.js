4221eef2e45992a6b8d8d6cc6bd64b57041901e0
Santiago <santiagoalamedasanchez@gmail.com>
2026-06-30 23:15:17 -0600

    Publicando con pnpm build

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // Esta línea le indica a Vite que los archivos de tu web
+  // deben buscarse de forma relativa, no absoluta.
+  base: './', 
+})
